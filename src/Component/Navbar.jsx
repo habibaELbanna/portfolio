32aaacdb0d991faf97097c0eb2951fb2de29e5d9
habibaELbanna <khaledhabiba267@gmail.com>
2025-11-28 20:19:35 +0200

    added burger menu in responsive

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import './Navbar.css';
 import logo from '../Assets/imgs/logo.svg';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="portfolio-navigation-header">
       
       <div className="portfolio-logo-wrapper">
-        <ScrollLink to="home" smooth={true} duration={600}>
+        <ScrollLink to="home" smooth={true} duration={600} onClick={closeMenu}>
           <img src={logo} alt="logo" style={{ cursor: "pointer" }} />
         </ScrollLink>
       </div>
 
-      <nav className="portfolio-main-navigation">
+
+      <button 
+        className={`burger-menu ${isMenuOpen ? 'active' : ''}`}
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        <span className="burger-line"></span>
+        <span className="burger-line"></span>
+        <span className="burger-line"></span>
+      </button>
+
+   
+      <nav className={`portfolio-main-navigation ${isMenuOpen ? 'menu-open' : ''}`}>
         
         <ScrollLink 
           to="home" 
           smooth={true} 
           duration={600}
           className="portfolio-nav-link"
+          onClick={closeMenu}
         >
           HOME
         </ScrollLink>
@@ -29,18 +52,17 @@ const Navbar = () => {
           smooth={true} 
           duration={600}
           className="portfolio-nav-link"
+          onClick={closeMenu}
         >
           GRAPHIC DESIGN
         </ScrollLink>
 
-       
-
-     
         <ScrollLink 
           to="webapp" 
           smooth={true} 
           duration={600}
           className="portfolio-nav-link"
+          onClick={closeMenu}
         >
           WEB / APP
         </ScrollLink>
@@ -50,6 +72,7 @@ const Navbar = () => {
           smooth={true} 
           duration={600}
           className="portfolio-nav-link"
+          onClick={closeMenu}
         >
           CODING
         </ScrollLink>
@@ -59,6 +82,7 @@ const Navbar = () => {
           smooth={true} 
           duration={600}
           className="portfolio-nav-link"
+          onClick={closeMenu}
         >
           ABOUT ME
         </ScrollLink>
@@ -68,11 +92,18 @@ const Navbar = () => {
           smooth={true} 
           duration={600}
           className="portfolio-nav-link"
+          onClick={closeMenu}
         >
           CONTACT
         </ScrollLink>
 
       </nav>
+
+      {/* Overlay for mobile menu */}
+      <div 
+        className={`menu-overlay ${isMenuOpen ? 'active' : ''}`}
+        onClick={closeMenu}
+      ></div>
 
     </header>
   );
